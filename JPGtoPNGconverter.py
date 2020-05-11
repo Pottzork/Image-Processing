7ec42037822172168a44cc8beb1d6871c2e22612
André Pontes <33063611+Pottzork@users.noreply.github.com>
2020-05-11 16:50:50 +0200

    Added instructions

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,6 +2,9 @@ import sys
 import os
 from PIL import Image
 
+# To run this script. Type in terminal:
+# Python JPGtoPNGconverter.py path_where_images_are_Stored path_to_newfolder
+# For example: Python JPGtoPNGconverter.py ./Pokedex/ desktop/new/
 
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
